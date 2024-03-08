f5a06f038d47aecc7398263ce1908e8dd300d141
chomusuke9072 <chomusuke9072@gmail.com>
2024-03-08 11:20:50 +0900

    🎉 add TodoList,sampleTodo 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import TodoForm from "./components/todo/TodoForm";
+import TodoList from "./components/todo/TodoList";
 
 const App = () => {
   return (
     <main>
       <TodoForm />
+      <TodoList todoTitle="In Progress" todos={[]} />
+      <TodoList todoTitle="Done" todos={[]} />
     </main>
   );
 };
---
+++ src/assets/sampleTodo.ts
@@ -0,0 +1,9 @@
+import { Todo } from "../types/todo-type";
+
+export const sampleTodo: Todo = {
+  id: "1",
+  title: "Sample Todo",
+  content: "This is a sample todo.",
+  isDone: false,
+  deadline: new Date().toDateString(),
+};
